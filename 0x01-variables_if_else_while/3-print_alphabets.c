7cb7dbb82b4db65f4b55efacb7f57578adf5cc16
kylead10 <adiokayode111@gmail.com>
2022-04-08 13:09:43 -0700

    print alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	char lower_case;
 	char upper_case;
+
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
@@ -23,7 +24,7 @@ int main(void)
 
 
 
-	putchar('\n');
+	putchar ('\n');
 
 	return (0);
 
