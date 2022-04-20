72f683c58f50486545ca285d12cb5ca9522b54a1
kylead10 <adiokayode111@gmail.com>
2022-04-20 06:26:45 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,20 +8,42 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
-	int aux = 0;
-	char 1temp;
-
-	while (*(s + i) != '\0')
-		i += 1;
-	i -= 1;
-
-	while (aux < 1)
+	int i = 0, length;
+	length = _strlen(s) - 1;
+	while (length > i)
 	{
-		ltemp = s[i];
-		s[i] = s[aux];
-		s[aux] = ltemp;
-		aux++;
-		i--;
+		swap_char(s + length, s + i);
+		i++;
+		length--;
 	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns lenght;
+ */
+
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
+
+/**
+ * swap_char - swap two characters
+ * @a: first character
+ * @b: second character
+ * Return: nothing
+ */
+
+void swap_char(char *a, char *b)
+{
+	char tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
