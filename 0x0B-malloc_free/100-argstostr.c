c19a5181f2f9553895271027676f0b98eca1d640
kylead10 <adiokayode111@gmail.com>
2022-05-05 08:38:03 -0700

    task 100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,7 +7,7 @@
  * Return: pointer to the new string
  */
 
-  char *argstostr(int ac, char **av)
+char *argstostr(int ac, char **av)
 {
 	char *new_string = NULL;
 	int k = 0, i = ac, j, sum = 0, temp = 0;
