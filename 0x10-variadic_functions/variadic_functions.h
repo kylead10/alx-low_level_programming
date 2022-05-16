56e856cf60d60ae181814d3a4c262eba9121a631
kylead10 <adiokayode111@gmail.com>
2022-05-16 04:19:04 -0700

    main

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,22 +1,22 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
+#ifndef VARFUNC_H
+#define VARFUNC_H
 #include <stdarg.h>
-#include <stdio.h>
-
-typedef struct printer{
-char *symbol;
-void (*print)(va_list arg);
-} printer_t;
-
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
 void print_all(const char * const format, ...);
+
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+} type_t;
 
 #endif
