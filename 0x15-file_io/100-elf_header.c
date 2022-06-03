ae02e56286932212ea8fccdd6beb359a5de42286
kylead10 <adiokayode111@gmail.com>
2022-06-03 04:32:28 -0700

    task 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -32,7 +32,7 @@ void check_elf(unsigned char *e_ident)
 		if (e_ident[index] != 127 &&
 	            e_ident[index] != 'E' &&
 		    e_ident[index] != 'L' &&
-                    e_ident[index] != 'F')
+                    e_ident[index] != 'F' )
 		{
 			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
